eae8755346b635a60e5a620d58adceef74879510
W-A-James <warren.james.dev@gmail.com>
2023-11-01 09:55:13 -0400

    update config files

--- server/test/message.test.ts
+++ server/test/message.test.ts
@@ -1,16 +1,16 @@
-import * as dgram from 'node:dgram';
-import { CommandMessage } from '../src/message.js'
+import { createSocket } from 'node:dgram';
+import { CommandMessage } from '../src/message'
 
 
-const onMessage = new CommandMessage('on', {});
-const offMessage = new CommandMessage('off', {});
+const onMessage = new CommandMessage('on');
+const offMessage = new CommandMessage('off');
 const rainbowMessage = new CommandMessage('rainbow', { delta: 4096, brightness: 128 });
-const chasingMessage = new CommandMessage('chasing', { hueDelta: 1000, brightness: 127 });
+const chasingMessage = new CommandMessage('chasing', { hueDelta: 100, hueWidth:20_000, brightness: 127 });
 const solidMessage = new CommandMessage('solid', { hue: 20_000, brightness: 129 });
 const breathingMessage = new CommandMessage('breathing', { delta: 2, hue: 8900, brightness: 130 });
 
 (async () => {
-  const server = dgram.createSocket('udp4');
+  const server = createSocket('udp4');
   server.connect(9000, '192.168.1.220', async () => {
     server.on('message', (msg, rinfo) => {
       console.log(msg.toString('utf8'));
