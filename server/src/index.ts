189da5b0067b73a9e35365c917effe75cd11c9da
W-A-James <warren.james.dev@gmail.com>
2023-10-31 22:06:59 -0400

    convert to TS

---
+++ server/src/index.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+import { MCU } from './mcu_interface';
+import { CommandMessage } from './message';
+
+dotenv.config();
+
+const app = express();
+app.use(express.static('./public'));
+app.use(express.json());
+
+const EXPRESS_TCP_PORT = Number(process.env.EXPRESS_PORT);
+const LOCAL_UDP_PORT = Number(process.env.LOCAL_UDP_PORT);
+const MCU_IP = process.env.MCU_IP;
+const MCU_PORT = Number(process.env.MCU_PORT);
+
+let mcu: MCU;
+
+app.post('/api', (req, res) => {
+  let command: CommandMessage;
+  try {
+    command = new CommandMessage(req.body.op, req.body.opts);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ ok: false, message: (e as Error).message ?? undefined });
+    return;
+  }
+
+  mcu.sendCommand(command)
+    .then(() => {
+      res.status(200).json({ ok: true });
+    },
+    (e: Error) => {
+      console.error(e);
+      res.status(500).json({ ok: false, message: e.message });
+    });
+});
+
+// Query MCU status
+app.get('/api', (_, res) => {
+  mcu.sendStatusRequest().then(
+    () => {
+      res.status(200).json({ ok: true, status: mcu.currentStatus });
+    },
+    (e: Error) => {
+      console.error(e);
+      res.status(500).json({ ok: false, message: e.message });
+    }
+  );
+});
+
+const server = app.listen(EXPRESS_TCP_PORT, () => {
+  console.log(`HTTP server listening on port ${EXPRESS_TCP_PORT}`);
+
+  if (MCU_IP === undefined || Number.isNaN(LOCAL_UDP_PORT) || Number.isNaN(MCU_PORT)) {
+    console.error('Environment variables LOCAL_UDP_PORT, MCU_PORT, and MCU_IP must be defined');
+    server.close();
+    return;
+  }
+
+  mcu = new MCU(LOCAL_UDP_PORT, MCU_IP, MCU_PORT);
+});
--- server/src/mcu_interface.ts
+++ server/src/mcu_interface.ts
@@ -1,15 +1,18 @@
 import dgram from 'node:dgram';
-import { CommandMessage, MCUStatusMessage } from './message.js';
+import { CommandMessage, MCUStatusMessage, type StatusMessage } from './message';
 
 export class MCU {
-  constructor(port, mcuIP, mcuPort) {
-    if (typeof port !== 'number' || Number.isNaN(port)) throw new Error(`port must be a number; got: ${port}`);
-    if (typeof mcuIP !== 'string') throw new Error(`mcuIP must be a string representing MCU IP address; got: ${mcuIP}`);
-    if (typeof mcuPort !== 'number' || Number.isNaN(mcuPort)) throw new Error(`mcuPort must be a number; got: ${mcuPort}`);
+  private readonly port: number;
+  private readonly mcuIP: string;
+  private readonly mcuPort: number;
+  private readonly server: dgram.Socket;
+  private _currentStatus: StatusMessage | undefined;
+
+  constructor (port: number, mcuIP: string, mcuPort: number) {
     this.port = port;
     this.mcuIP = mcuIP;
     this.mcuPort = mcuPort;
-    this.currentStatus = undefined;
+    this._currentStatus = undefined;
 
     this.server = dgram.createSocket('udp4');
 
@@ -23,7 +26,7 @@ export class MCU {
       }
       try {
         const status = new MCUStatusMessage(message).decode();
-        this.currentStatus = status;
+        this._currentStatus = status;
       } catch (e) {
         console.error(e);
       }
@@ -31,38 +34,37 @@ export class MCU {
     this.server.bind(this.port);
   }
 
-  /**
-    * @param message {CommandMessage}
-    * @returns {Promise<void>}
-    * */
-  async sendCommand(message) {
+  async sendCommand (message: CommandMessage): Promise<void> {
     const sendSocket = dgram.createSocket('udp4');
     await new Promise((resolve, reject) => {
       sendSocket.send(message.buffer, this.mcuPort, this.mcuIP, (err) => {
-        if (err) {
+        if (err != null) {
           sendSocket.close(() => { reject(err); });
         } else {
-          sendSocket.close(resolve);
+          sendSocket.close(() => { resolve(null); });
         }
       });
     });
   }
 
-  /** @returns {Promise<StatusMessage>} */
-  async sendStatusRequest() {
-    const message = new CommandMessage('status', {});
-    return await new Promise((resolve, reject) => {
+  async sendStatusRequest (): Promise<void> {
+    const message = new CommandMessage('status');
+    await new Promise((resolve, reject) => {
       this.server.send(message.buffer, this.mcuPort, this.mcuIP, (err) => {
-        if (err) {
+        if (err != null) {
           reject(err);
         } else {
-          resolve();
+          resolve(null);
         }
       });
     });
   }
 
-  close() {
+  get currentStatus (): StatusMessage | undefined {
+    return this._currentStatus;
+  }
+
+  close (): void {
     this.server.close();
   }
 };
