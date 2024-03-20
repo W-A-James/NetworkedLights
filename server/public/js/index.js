311136d150b6be75e1ecba2126dc36679df5e43f
W-A-James <warren.james.dev@gmail.com>
2024-03-20 01:23:26 -0400

    make sliders easier to use

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -44,20 +44,22 @@ const CHASING_DELTA_STATUS = document.getElementById('currentChasingDelta');
 const RAINBOW_DELTA_STATUS = document.getElementById('currentRainbowDelta');
 
 const ANIMATION_RADIO_INPUTS = document.getElementsByName('animation');
-const BRIGHTNESS_INPUT = document.getElementById('brightness');
-const HUE_INPUT = document.getElementById('hue');
-const BREATHING_DELTA_INPUT = document.getElementById('breathingDelta');
-const CHASING_HUE_WIDTH_INPUT = document.getElementById('chasingHueWidth');
-const CHASING_DELTA_INPUT = document.getElementById('chasingDelta');
-const RAINBOW_DELTA_INPUT = document.getElementById('rainbowDelta');
 
-const BRIGHTNESS_SPAN = document.getElementById('brightnessSpan');
-const HUE_SPAN = document.getElementById('hueSpan');
-const BREATHING_DELTA_SPAN = document.getElementById('breathingDeltaSpan');
-const CHASING_HUE_WIDTH_SPAN = document.getElementById('chasingHueWidthSpan');
-const CHASING_DELTA_SPAN = document.getElementById('chasingDeltaSpan');
-const RAINBOW_DELTA_SPAN = document.getElementById('rainbowDeltaSpan');
+const BRIGHTNESS_RANGE_INPUT = document.getElementById('brightness');
+const HUE_RANGE_INPUT = document.getElementById('hue');
+const BREATHING_DELTA_RANGE_INPUT = document.getElementById('breathingDelta');
+const CHASING_HUE_WIDTH_RANGE_INPUT = document.getElementById('chasingHueWidth');
+const CHASING_DELTA_RANGE_INPUT = document.getElementById('chasingDelta');
+const RAINBOW_DELTA_RANGE_INPUT = document.getElementById('rainbowDelta');
 
+const BRIGHTNESS_NUM_INPUT = document.getElementById('brightness-num');
+const HUE_NUM_INPUT = document.getElementById('hue-num');
+const BREATHING_DELTA_NUM_INPUT = document.getElementById('breathingDelta-num');
+const CHASING_HUE_WIDTH_NUM_INPUT = document.getElementById('chasingHueWidth-num');
+const CHASING_DELTA_NUM_INPUT = document.getElementById('chasingDelta-num');
+const RAINBOW_DELTA_NUM_INPUT = document.getElementById('rainbowDelta-num');
+
+const HUE_LABEL = document.getElementById('hue-label');
 function getMCUStatus() {
   const req = new Request('/api', { method: 'GET' });
 
@@ -81,8 +83,8 @@ function getMCUStatus() {
     .catch(console.error);
 }
 
-// refresh mcu status every 0.5 seconds
-setInterval(getMCUStatus, 500);
+// refresh mcu status every 0.25 seconds
+setInterval(getMCUStatus, 250);
 
 function sendDatatoMCU() {
   const selectedRadio = (() => {
@@ -92,17 +94,17 @@ function sendDatatoMCU() {
   })();
 
   /** @type Command */
-  const body = { op: selectedRadio.id, opts: { hue: HUE_INPUT.value, brightness: BRIGHTNESS_INPUT.value } };
+  const body = { op: selectedRadio.id, opts: { hue: HUE_RANGE_INPUT.value, brightness: BRIGHTNESS_RANGE_INPUT.value } };
   switch (selectedRadio.id) {
     case 'rainbow':
-      body.opts.delta = RAINBOW_DELTA_INPUT.value;
+      body.opts.delta = RAINBOW_DELTA_RANGE_INPUT.value;
       break;
     case 'breathing':
-      body.opts.delta = BREATHING_DELTA_INPUT.value;
+      body.opts.delta = BREATHING_DELTA_RANGE_INPUT.value;
       break;
     case 'chasing':
-      body.opts.hueWidth = CHASING_HUE_WIDTH_INPUT.value;
-      body.opts.hueDelta = CHASING_DELTA_INPUT.value;
+      body.opts.hueWidth = CHASING_HUE_WIDTH_RANGE_INPUT.value;
+      body.opts.hueDelta = CHASING_DELTA_RANGE_INPUT.value;
       break;
     case 'solid':
       break;
@@ -142,15 +144,46 @@ toggleButton.addEventListener('click', (event) => {
     sendCommand(payload);
 });
 
-function connectSpanAndInput(input, span) {
-  input.addEventListener('change', () => {
-    span.innerText = input.value;
+function connectRangeAndNumInputs(range, num) {
+  range.addEventListener('change', () => {
+    updateInputValue(range, num);
+  });
+
+  num.addEventListener('change', () => {
+    updateInputValue(num, range);
   });
 }
 
-connectSpanAndInput(BRIGHTNESS_INPUT, BRIGHTNESS_SPAN);
-connectSpanAndInput(HUE_INPUT, HUE_SPAN);
-connectSpanAndInput(BREATHING_DELTA_INPUT, BREATHING_DELTA_SPAN);
-connectSpanAndInput(CHASING_HUE_WIDTH_INPUT, CHASING_HUE_WIDTH_SPAN);
-connectSpanAndInput(CHASING_DELTA_INPUT, CHASING_DELTA_SPAN);
-connectSpanAndInput(RAINBOW_DELTA_INPUT, RAINBOW_DELTA_SPAN);
+function updateInputValue(source, dest) {
+  dest.value = source.value;
+}
+
+connectRangeAndNumInputs(BRIGHTNESS_RANGE_INPUT, BRIGHTNESS_NUM_INPUT);
+updateInputValue(BRIGHTNESS_RANGE_INPUT, BRIGHTNESS_NUM_INPUT);
+
+connectRangeAndNumInputs(HUE_RANGE_INPUT, HUE_NUM_INPUT);
+updateInputValue(HUE_RANGE_INPUT, HUE_NUM_INPUT);
+function changeHueBackgroundCoulour(event) {
+  const hueVal = Number(event.target.value);
+  if (Number.isFinite(hueVal) && hueVal <= 65536) {
+    const hueDegrees = (hueVal / 65538) * 360;
+    const hsl = `hsl(${Math.fround(hueDegrees)}deg 100% 50%)`;
+    console.log(`Set bg colour to ${hsl}`);
+    HUE_LABEL.setAttribute("style", `background-color: ${hsl} !important`);
+  }
+}
+HUE_RANGE_INPUT.addEventListener('change', changeHueBackgroundCoulour);
+HUE_NUM_INPUT.addEventListener('change', changeHueBackgroundCoulour);
+
+
+connectRangeAndNumInputs(BREATHING_DELTA_RANGE_INPUT, BREATHING_DELTA_NUM_INPUT);
+updateInputValue(BREATHING_DELTA_RANGE_INPUT, BREATHING_DELTA_NUM_INPUT);
+
+connectRangeAndNumInputs(CHASING_HUE_WIDTH_RANGE_INPUT, CHASING_HUE_WIDTH_NUM_INPUT);
+updateInputValue(CHASING_HUE_WIDTH_RANGE_INPUT, CHASING_HUE_WIDTH_NUM_INPUT);
+
+connectRangeAndNumInputs(CHASING_DELTA_RANGE_INPUT, CHASING_DELTA_NUM_INPUT);
+updateInputValue(CHASING_DELTA_RANGE_INPUT, CHASING_DELTA_NUM_INPUT);
+
+connectRangeAndNumInputs(RAINBOW_DELTA_RANGE_INPUT, RAINBOW_DELTA_NUM_INPUT);
+updateInputValue(RAINBOW_DELTA_RANGE_INPUT, RAINBOW_DELTA_NUM_INPUT);
