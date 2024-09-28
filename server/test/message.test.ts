ad746db00c731f01f3282c8dbc57aff4190287d6
W-A-James <warren.james.dev@gmail.com>
2024-09-28 04:43:35 -0400

    test cleanup

--- server/test/message.test.ts
+++ server/test/message.test.ts
@@ -1,30 +1,44 @@
 import { createSocket } from 'node:dgram';
+import { setTimeout } from 'node:timers/promises';
+import { join } from 'node:path';
+
+import * as dotenv from 'dotenv';
+
 import { CommandMessage } from '../src/message'
 
+dotenv.config({
+  path: join(__dirname, '.env')
+});
 
-const onMessage = new CommandMessage('on');
-const offMessage = new CommandMessage('off');
-const rainbowMessage = new CommandMessage('rainbow', { delta: 4096, brightness: 128 });
-const chasingMessage = new CommandMessage('chasing', { hueDelta: 100, hueWidth:20_000, brightness: 127 });
-const solidMessage = new CommandMessage('solid', { hue: 20_000, brightness: 129 });
-const breathingMessage = new CommandMessage('breathing', { delta: 2, hue: 8900, brightness: 130 });
+const port = Number(process.env.MCU_PORT);
+const ip = process.env.MCU_IP as string;
+
+const messages = [
+  new CommandMessage('on'),
+  new CommandMessage('off'),
+  new CommandMessage('rainbow', { delta: 4096, brightness: 128 }),
+  new CommandMessage('chasing', { hueDelta: 100, hueWidth: 20_000, brightness: 127 }),
+  new CommandMessage('solid', { hue: 20_000, brightness: 129 }),
+  new CommandMessage('breathing', { delta: 2, hue: 8900, brightness: 130 })
+];
 
 (async () => {
-  const server = createSocket('udp4');
-  server.connect(9000, '192.168.1.220', async () => {
-    server.on('message', (msg, rinfo) => {
-      console.log(msg.toString('utf8'));
+  const sock = createSocket('udp4');
+  sock.connect(port, ip, async () => {
+    sock.on('message', (msg, rinfo) => {
+      console.log(`Got ${msg.toString('hex')} from ${rinfo.address}:${rinfo.port}`);
     });
 
-    for (const message of [onMessage, offMessage, chasingMessage, rainbowMessage, solidMessage, breathingMessage]) {
-      server.send(message.buffer, (err, n) => {
+    for (const message of messages) {
+      console.log(`Sending '${message.op}' command to ${ip}:${port}...`);
+      sock.send(message.buffer, (err, n) => {
         if (err) throw new Error('fail');
-        console.log(`sent ${n} bytes: ${message.buffer.toString('hex')}`);
+        console.log(`- sent ${n} bytes: ${message.buffer.toString('hex')}`);
       });
 
-      await new Promise(resolve => setTimeout(resolve, 5000));
+      await setTimeout(5000);
     }
-    server.close();
+    sock.close();
   });
 
 })();
