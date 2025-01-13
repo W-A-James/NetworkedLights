a96fdd5c6ef238184ea45765071fd3a9343d6fbd
W-A-James <warren.james.dev@gmail.com>
2025-01-12 20:16:28 -0500

    remove old server

--- server/src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function log (...args: any[]): void {
-  const date = new Date();
-  console.log(`[${date.toISOString()}]`, ...args);
-}
-
-export function error (...args: any[]): void {
-  const date = new Date();
-  console.error(`[${date.toISOString()}]`, ...args);
-}
